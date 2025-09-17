f98bb525b2b0acf7b5c7614b0a423ba20a7326ea
Rita Mordio <ritamordio0716@gmail.com>
2025-09-17 13:03:57 +0900

    줄거리 스킵 기능 추가

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,21 @@
-const dataUia = ['player-skip-intro', 'next-episode-seamless-button']
+const dataUia = ['player-skip-intro', 'next-episode-seamless-button', 'player-skip-recap']
 
 const settingObject = {
   introSkip: false,
-  outroSkip: false
+  outroSkip: false,
+  recapSkip: false
 }
 
-chrome.storage.local.get(['introSkip', 'outroSkip'], ({ introSkip, outroSkip }) => {
+chrome.storage.local.get(['introSkip', 'outroSkip', 'recapSkip'], ({ introSkip, outroSkip, recapSkip }) => {
   settingObject.introSkip = introSkip
   settingObject.outroSkip = outroSkip
+  settingObject.recapSkip = recapSkip
 });
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
   settingObject.introSkip = changes.introSkip.newValue;
   settingObject.outroSkip = changes.outroSkip.newValue;
+  settingObject.recapSkip = changes.recapSkip.newValue;
 });
 
 const skipHandler = () => {
@@ -25,6 +28,7 @@ const skipHandler = () => {
     if(dataUia.includes(attribute)) {
       if(attribute === 'player-skip-intro' && settingObject.introSkip) button.click()
       if(attribute === 'next-episode-seamless-button' && settingObject.outroSkip) button.click()
+      if(attribute === 'player-skip-recap' && settingObject.recapSkip) button.click()
     }
   })
 }
